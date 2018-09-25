4bd8b3a523becda1fd4ad6053eb7c8531c51cddd
Zhipeng Wang <pengw@hpe.com>
2018-09-25 18:23:44 +0800

    add morew rest methods

--- Startup.cs
+++ Startup.cs
@@ -36,6 +36,8 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
             app.UseMvc();
         }
     }
